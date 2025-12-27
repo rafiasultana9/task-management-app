77c6795bd3f53df85753ab84b95c3b2d2502ff95
Rafia-Sultana <rafiasultana0097@gmail.com>
2025-12-27 23:31:14 +0600

    working on auth apis

---
+++ app/api/auth/login/route.ts
@@ -0,0 +1,70 @@
+
+import { prisma } from "@/lib/prisma";
+import bcrypt from "bcryptjs";
+import { NextRequest, NextResponse } from "next/server";
+import z from "zod";
+import { SignJWT } from "jose";
+
+
+const loginSchema = z.object({
+    email: z.string().email(),
+    password: z.string().min(6),
+});
+
+export async function POST(request: NextRequest) {
+    try {
+        const body = await request.json();
+        const { email, password } = loginSchema.parse(body);
+
+        const user = await prisma.user.findUnique({ where: { email } });
+
+        if (!user || !user.password) {
+            return NextResponse.json({ error: "Invalid Email or password" }), { status: 401 }
+        }
+
+        const isValidPassword = await bcrypt.compare(password, user.password);
+
+        if (!isValidPassword) {
+            return NextResponse.json({ error: "Invalid email or password" }, { status: 401 })
+        }
+
+        const secret = new TextEncoder().encode(process.env.JWT_Secret || "your-secret-key-change-this");
+        const token = await new SignJWT({
+
+            userId: user.id,
+            email: user.email,
+            role: user.role
+        }).setProtectedHeader({ alg: "HS256" })
+            .setExpirationTime("7d")
+            .sign(secret);
+
+        const response = NextResponse.json({
+            message: "Login Successful",
+            user: {
+                id: user.id,
+                name: user.name,
+                email: user.email,
+                role: user.role
+            }
+        },
+            { status: 200 }
+        )
+
+        response.cookies.set("token", token, {
+            httpOnly: true, secure: true,
+            sameSite: "lax",
+            maxAge: 60 * 60 * 24 * 7
+        })
+        return response;
+    } catch (error) {
+     if(error instanceof z.ZodError){
+        return NextResponse.json(
+            {error:"Validation Failed", details:error.issues},{status:400}
+        )
+     };
+     console.error("login error", error);
+     return NextResponse.json({
+        error:"Internal Server Error"
+     }, {status:500})
+    }
+}
